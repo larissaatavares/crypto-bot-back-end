f13a1507b94115b27a087756cddf09d9192a0ca1
Larissa Tavares <larissa.a.tavares@outlook.com>
2022-12-13 21:29:36 -0300

    app index

--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
 import app from './server.js';
 import './controllers/index.js';
+import StrategyManager from './strategies/StrategyManager.js';
 
-// start all strategies
-//import Strategy from './database/Strategy.js';
-//const strategies = await Strategy.findAll();
-//strategies.filter() // everyone that isn't a test and run them
-
+StrategyManager.startAll();
 app.listen(3000);
