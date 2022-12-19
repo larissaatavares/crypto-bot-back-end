98005881a5f60d2a360041a70ae87936e4ad1723
Larissa Tavares <larissa.a.tavares@outlook.com>
2022-12-19 09:10:19 -0300

    Update dependencies.

--- src/websockets/index.js
+++
@@ -1,8 +0,0 @@
-import { Server } from 'socket.io';
-const io = new Server(3001);
-export default io;
-
-io.on('connection', client => {
-    console.log('connected!');
-    client.emit('connected!');
-});
