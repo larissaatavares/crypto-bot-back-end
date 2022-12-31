df46d3d30e8512d5e8cb09a0ed2c79275fcdf7fe
Larissa Tavares <larissa.a.tavares@outlook.com>
2022-12-31 20:55:39 -0300

    Removed OCOOrder

--- src/strategies/STRATEGY_CLASSES.js
+++ src/strategies/STRATEGY_CLASSES.js
@@ -1,11 +1,9 @@
-import OCOOrder from './OCOOrder.js';
 import TrailingOrder from './TrailingOrder.js';
 import StoplossOrder from './StoplossOrder.js';
 import MarketOrder from './MarketOrder.js';
 import BaseStrategy from './BaseStrategy.js';
 
 export default class STRATEGY_CLASSES { 
-    static OCOOrder = OCOOrder;
     static TrailingOrder = TrailingOrder;
     static StoplossOrder = StoplossOrder;
     static MarketOrder = MarketOrder;
